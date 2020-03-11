8a42724b0ac1f5879842d4c686df4619d9d77ecc
ZBJim <19714491@qq.com>
2020-03-11 16:28:57 +0800

    初步完成Demo

--- app/src/main/java/com/example/coolweather/MainActivity.kt
+++ app/src/main/java/com/example/coolweather/MainActivity.kt
@@ -1,12 +1,64 @@
 package com.example.coolweather
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import org.litepal.LitePal
+import android.content.Intent
+import android.preference.PreferenceManager
+import android.content.SharedPreferences
+import androidx.core.app.ComponentActivity
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+   //     LitePal.initialize(this)
+        requestPermission()
+        val prefs = PreferenceManager.getDefaultSharedPreferences(this)
+        if (prefs.getString("weather", null) != null) {
+            val intent = Intent(this, WeatherActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.INTERNET), 1)
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.INTERNET
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.INTERNET), 1)
+        } else {
+        }
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            1 -> if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) { //同意权限申请
+                Toast.makeText(this, "权限允许", Toast.LENGTH_SHORT).show()
+            } else { //拒绝权限申请
+                Toast.makeText(this, "权限被拒绝了", Toast.LENGTH_SHORT).show()
+            }
+            else -> {
+            }
+        }
     }
 }
